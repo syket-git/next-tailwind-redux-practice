97cadc1916334df2afaa047c6c9ea31879f2fc10
syket <syketweb@gmail.com>
2022-01-04 11:21:57 +0600

    not found message added

--- pages/list/index.js
+++ pages/list/index.js
@@ -2,6 +2,8 @@ import HomeLayout from '../../layouts/HomeLayout';
 import { useSelector } from 'react-redux';
 import { todoSelector } from '../../store/slices/todoSlice';
 import ListItem from '../../components/ListItem/index';
+import SecondaryButton from '../../components/form/Button/SecondaryButton';
+import router from 'next/router';
 
 const List = () => {
   const { list } = useSelector(todoSelector);
@@ -9,9 +11,18 @@ const List = () => {
   return (
     <HomeLayout>
       <div className="container mt-10 mx-auto">
-        {list.map((item, index) => (
-          <ListItem key={index} item={item} index={index} />
-        ))}
+        {list?.length > 0 ? (
+          list.map((item, index) => (
+            <ListItem key={index} item={item} index={index} />
+          ))
+        ) : (
+          <div className="text-center space-y-3">
+            <p>Sorry, no item added yet</p>
+            <SecondaryButton onClick={() => router.push('/')}>
+              Add new todo
+            </SecondaryButton>
+          </div>
+        )}
       </div>
     </HomeLayout>
   );
